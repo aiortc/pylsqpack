79b46ca959500656eb58b394bbc8515231db7152
Jeremy Lainé <jeremy.laine@m4x.org>
2019-07-21 23:43:06 +0200

    [decoder] start handling blocked streams

--- src/module.c
+++ src/module.c
@@ -8,69 +8,47 @@
 #define HDR_BUF_SZ 4096
 #define PREFIX_MAX_SIZE 16
 
-// DECODER
+static PyObject *StreamBlocked;
 
-typedef struct {
-    PyObject_HEAD
-    struct lsqpack_dec dec;
-    unsigned char dec_buf[DEC_BUF_SZ];
-} DecoderObject;
+struct header_block {
+    STAILQ_ENTRY(header_block) entries;
 
-static int
-Decoder_init(DecoderObject *self, PyObject *args, PyObject *kwargs)
-{
-    char *kwlist[] = {"max_table_capacity", "blocked_streams", NULL};
-    unsigned max_table_capacity, blocked_streams;
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "II", kwlist, &max_table_capacity, &blocked_streams))
-        return -1;
-
-    lsqpack_dec_init(&self->dec, NULL, max_table_capacity, blocked_streams, NULL);
-    return 0;
-}
-
-static void
-Decoder_dealloc(DecoderObject *self)
-{
-    lsqpack_dec_cleanup(&self->dec);
-}
-
-static PyObject*
-Decoder_feed_encoder(DecoderObject *self, PyObject *args)
-{
-    const unsigned char *data = NULL;
-    int data_len = 0;
-
-    if (!PyArg_ParseTuple(args, "y#", &data, &data_len))
-        return NULL;
-
-    if (lsqpack_dec_enc_in(&self->dec, data, data_len) < 0) {
-        PyErr_SetString(PyExc_RuntimeError, "lsqpack_dec_enc_in failed");
-        return NULL;
-    }
-
-    Py_RETURN_NONE;
-}
-
-static PyObject*
-Decoder_feed_header(DecoderObject *self, PyObject *args)
-{
-    uint64_t stream_id;
-    const unsigned char *data = NULL;
-    int data_len = 0;
+    int blocked:1;
+    unsigned char *data;
+    size_t data_len;
+    const unsigned char *data_ptr;
     struct lsqpack_header_set *hset;
-    struct lsqpack_header *header;
-    PyObject *list, *tuple, *name, *value;
-    size_t dec_len = DEC_BUF_SZ;
-    enum lsqpack_read_header_status status;
+    uint64_t stream_id;
+};
 
-    if (!PyArg_ParseTuple(args, "Ky#", &stream_id, &data, &data_len))
-        return NULL;
+static struct header_block *header_block_new(size_t stream_id, const unsigned char *data, size_t data_len)
+{
+    struct header_block *hblock = malloc(sizeof(struct header_block));
+    memset(hblock, 0, sizeof(*hblock));
+    hblock->data = malloc(data_len);
+    hblock->data_len = data_len;
+    hblock->data_ptr = hblock->data;
+    memcpy(hblock->data, data, data_len);
+    hblock->stream_id = stream_id;
+    return hblock;
+}
 
-    status = lsqpack_dec_header_in(&self->dec, NULL, stream_id, data_len, &data, data_len, &hset, self->dec_buf, &dec_len);
-    if (status != LQRHS_DONE) {
-        PyErr_Format(PyExc_RuntimeError, "lsqpack_dec_header_in failed (%d)", status);
-        return NULL;
+static void header_block_free(struct header_block *hblock)
+{
+    free(hblock->data);
+    hblock->data = 0;
+    hblock->data_ptr = 0;
+    if (hblock->hset) {
+        lsqpack_dec_destroy_header_set(hblock->hset);
+        hblock->hset = 0;
     }
+    free(hblock);
+}
+
+static PyObject *hset_to_headers(struct lsqpack_header_set *hset)
+{
+    PyObject *list, *tuple, *name, *value;
+    struct lsqpack_header *header;
 
     list = PyList_New(hset->qhs_count);
     for (size_t i = 0; i < hset->qhs_count; ++i) {
@@ -82,7 +60,191 @@ Decoder_feed_header(DecoderObject *self, PyObject *args)
         Py_DECREF(value);
         PyList_SET_ITEM(list, i, tuple);
     }
-    lsqpack_dec_destroy_header_set(hset);
+    return list;
+}
+
+static void header_unblocked(void *opaque) {
+    struct header_block *hblock = opaque;
+    hblock->blocked = 0;
+}
+
+// DECODER
+
+typedef struct {
+    PyObject_HEAD
+    struct lsqpack_dec dec;
+    unsigned char dec_buf[DEC_BUF_SZ];
+    STAILQ_HEAD(, header_block) pending_blocks;
+} DecoderObject;
+
+static int
+Decoder_init(DecoderObject *self, PyObject *args, PyObject *kwargs)
+{
+    char *kwlist[] = {"max_table_capacity", "blocked_streams", NULL};
+    unsigned max_table_capacity, blocked_streams;
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "II", kwlist, &max_table_capacity, &blocked_streams))
+        return -1;
+
+    lsqpack_dec_init(&self->dec, NULL, max_table_capacity, blocked_streams, header_unblocked);
+
+    STAILQ_INIT(&self->pending_blocks);
+
+    return 0;
+}
+
+static void
+Decoder_dealloc(DecoderObject *self)
+{
+    struct header_block *hblock;
+
+    lsqpack_dec_cleanup(&self->dec);
+
+    while (!STAILQ_EMPTY(&self->pending_blocks)) {
+        hblock = STAILQ_FIRST(&self->pending_blocks);
+        STAILQ_REMOVE_HEAD(&self->pending_blocks, entries);
+        header_block_free(hblock);
+    }
+}
+
+static PyObject*
+Decoder_feed_encoder(DecoderObject *self, PyObject *args, PyObject *kwargs)
+{
+    char *kwlist[] = {"data", NULL};
+    const unsigned char *data = NULL;
+    int data_len = 0;
+    PyObject *list, *value;
+    struct header_block *hblock;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#", kwlist, &data, &data_len))
+        return NULL;
+
+    if (lsqpack_dec_enc_in(&self->dec, data, data_len) < 0) {
+        PyErr_SetString(PyExc_RuntimeError, "lsqpack_dec_enc_in failed");
+        return NULL;
+    }
+
+    list = PyList_New(0);
+    STAILQ_FOREACH(hblock, &self->pending_blocks, entries) {
+        if (!hblock->blocked) {
+            value = PyLong_FromUnsignedLongLong(hblock->stream_id);
+            PyList_Append(list, value);
+        }
+    }
+    return list;
+}
+
+static PyObject*
+Decoder_feed_header(DecoderObject *self, PyObject *args, PyObject *kwargs)
+{
+    char *kwlist[] = {"stream_id", "data", NULL};
+    uint64_t stream_id;
+    const unsigned char *data = NULL;
+    int data_len = 0;
+    PyObject *list;
+    size_t dec_len = DEC_BUF_SZ;
+    enum lsqpack_read_header_status status;
+    struct header_block *hblock;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "Ky#", kwlist, &stream_id, &data, &data_len))
+        return NULL;
+
+    // check there is no header block for the stream
+    STAILQ_FOREACH(hblock, &self->pending_blocks, entries) {
+        if (hblock->stream_id == stream_id) {
+            PyErr_Format(PyExc_ValueError, "a header block for stream %d already exists", stream_id);
+            return NULL;
+        }
+    }
+    hblock = header_block_new(stream_id, data, data_len);
+
+    status = lsqpack_dec_header_in(
+        &self->dec,
+        hblock,
+        stream_id,
+        hblock->data_len,
+        &hblock->data_ptr,
+        hblock->data_len,
+        &hblock->hset,
+        self->dec_buf,
+        &dec_len
+    );
+
+    if (status == LQRHS_BLOCKED || status == LQRHS_NEED) {
+        hblock->blocked = 1;
+        STAILQ_INSERT_TAIL(&self->pending_blocks, hblock, entries);
+        PyErr_Format(StreamBlocked, "stream %d is blocked", stream_id);
+        return NULL;
+    } else if (status != LQRHS_DONE) {
+        PyErr_Format(PyExc_RuntimeError, "lsqpack_dec_header_in for stream %d failed (%d)", stream_id, status);
+        STAILQ_REMOVE(&self->pending_blocks, hblock, header_block, entries);
+        header_block_free(hblock);
+        return NULL;
+    }
+
+    list = hset_to_headers(hblock->hset);
+    header_block_free(hblock);
+
+    return PyTuple_Pack(
+        2,
+        PyBytes_FromStringAndSize((const char*)self->dec_buf, dec_len),
+        list
+    );
+}
+
+static PyObject*
+Decoder_resume_header(DecoderObject *self, PyObject *args, PyObject *kwargs)
+{
+    char *kwlist[] = {"stream_id", NULL};
+    uint64_t stream_id;
+    PyObject *list;
+    size_t dec_len = DEC_BUF_SZ;
+    enum lsqpack_read_header_status status;
+    struct header_block *hblock;
+    int found = 0;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "K", kwlist, &stream_id))
+        return NULL;
+
+    // find the header block for the stream
+    STAILQ_FOREACH(hblock, &self->pending_blocks, entries) {
+        if (hblock->stream_id == stream_id) {
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        PyErr_Format(PyExc_ValueError, "no pending header block for stream %d", stream_id);
+        return NULL;
+    }
+
+    if (hblock->blocked) {
+        status = LQRHS_BLOCKED;
+    } else {
+        status = lsqpack_dec_header_read(
+            &self->dec,
+            hblock,
+            &hblock->data_ptr,
+            hblock->data_len - (hblock->data_ptr - hblock->data),
+            &hblock->hset,
+            self->dec_buf,
+            &dec_len
+        );
+    }
+
+    if (status == LQRHS_BLOCKED || status == LQRHS_NEED) {
+        hblock->blocked = 1;
+        PyErr_Format(StreamBlocked, "stream %d is blocked", stream_id);
+        return NULL;
+    } else if (status != LQRHS_DONE) {
+        PyErr_Format(PyExc_RuntimeError, "lsqpack_dec_header_in for stream %d failed (%d)", stream_id, status);
+        STAILQ_REMOVE(&self->pending_blocks, hblock, header_block, entries);
+        header_block_free(hblock);
+        return NULL;
+    }
+
+    list = hset_to_headers(hblock->hset);
+    STAILQ_REMOVE(&self->pending_blocks, hblock, header_block, entries);
+    header_block_free(hblock);
 
     return PyTuple_Pack(
         2,
@@ -92,8 +254,9 @@ Decoder_feed_header(DecoderObject *self, PyObject *args)
 }
 
 static PyMethodDef Decoder_methods[] = {
-    {"feed_encoder", (PyCFunction)Decoder_feed_encoder, METH_VARARGS, "Feed data from the encoder stream."},
-    {"feed_header", (PyCFunction)Decoder_feed_header, METH_VARARGS, "Feed data from a data stream."},
+    {"feed_encoder", (PyCFunction)Decoder_feed_encoder, METH_VARARGS | METH_KEYWORDS, "Feed data from the encoder stream."},
+    {"feed_header", (PyCFunction)Decoder_feed_header, METH_VARARGS | METH_KEYWORDS, "Start decoding a header block and return headers."},
+    {"resume_header", (PyCFunction)Decoder_resume_header, METH_VARARGS | METH_KEYWORDS, "Continue decoding a header block and return headers."},
     {NULL}
 };
 
@@ -331,6 +494,10 @@ PyInit_pylsqpack(void)
     m = PyModule_Create(&moduledef);
     if (m == NULL)
         return NULL;
+
+    StreamBlocked = PyErr_NewException(MODULE_NAME ".StreamBlocked", PyExc_ValueError, NULL);
+    Py_INCREF(StreamBlocked);
+    PyModule_AddObject(m, "StreamBlocked", StreamBlocked);
 
     DecoderType.tp_new = PyType_GenericNew;
     if (PyType_Ready(&DecoderType) < 0)
---
+++ tests/test_decoder.py
@@ -0,0 +1,64 @@
+import binascii
+from unittest import TestCase
+
+from pylsqpack import Decoder, StreamBlocked
+
+
+class DecoderTest(TestCase):
+    def test_blocked_stream(self):
+        decoder = Decoder(0x100, 0x10)
+        stream_id = 0
+
+        # no streams unblocked
+        self.assertEqual(decoder.feed_encoder(b""), [])
+
+        # cannot resume non existent block
+        with self.assertRaises(ValueError):
+            decoder.resume_header(stream_id)
+
+        # the stream is blocked
+        with self.assertRaises(StreamBlocked):
+            decoder.feed_header(stream_id, binascii.unhexlify("0482d9101112"))
+
+        # the stream is still blocked
+        with self.assertRaises(StreamBlocked):
+            decoder.resume_header(stream_id)
+
+        # the stream becomes unblocked
+        self.assertEqual(
+            decoder.feed_encoder(
+                binascii.unhexlify(
+                    "3fe10168f2b14939d69ce84f8d9635e9ef2a12bd454dc69a659f6cf2b14939d6"
+                    "b505b161cc5a9385198fdad313c696dd6d5f4a082a65b6850400bea0837190dc"
+                    "138a62d1bf"
+                )
+            ),
+            [stream_id],
+        )
+
+        # the header is resumed
+        control, headers = decoder.resume_header(stream_id)
+        self.assertEqual(control, b"\x80")
+        self.assertEqual(
+            headers,
+            [
+                (b":status", b"200"),
+                (b"x-echo-host", b"fb.mvfst.net:4433"),
+                (b"x-echo-user-agent", b"aioquic"),
+                (b"date", b"Sun, 21 Jul 2019 21:31:26 GMT"),
+            ],
+        )
+
+        # free the decoder
+        del decoder
+
+    def test_blocked_stream_free(self):
+        decoder = Decoder(0x100, 0x10)
+        stream_id = 0
+
+        # the stream is blocked
+        with self.assertRaises(StreamBlocked):
+            decoder.feed_header(stream_id, binascii.unhexlify("0482d9101112"))
+
+        # free the decoder with pending block
+        del decoder
