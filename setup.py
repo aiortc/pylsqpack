f1d88ae600ab08195325f4531cfb960a01673f49
Jeremy Lainé <jeremy.laine@m4x.org>
2019-10-22 08:30:33 +0200

    0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme_file, encoding='utf-8') as f:
 
 setuptools.setup(
     name='pylsqpack',
-    version='0.3.0',
+    version='0.3.1',
     description='Python wrapper for the ls-qpack QPACK library',
     long_description=long_description,
     url='https://github.com/aiortc/pylsqpack',
