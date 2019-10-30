09dd2a5da48421d891638169ba93ea5226d01826
Jeremy Lainé <jeremy.laine@m4x.org>
2019-10-30 11:44:10 +0100

    [package] reformat setup.py using black

--- setup.py
+++ setup.py
@@ -3,44 +3,41 @@ import os.path
 import setuptools
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
-readme_file = os.path.join(root_dir, 'README.rst')
-with open(readme_file, encoding='utf-8') as f:
+readme_file = os.path.join(root_dir, "README.rst")
+with open(readme_file, encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='pylsqpack',
-    version='0.3.1',
-    description='Python wrapper for the ls-qpack QPACK library',
+    name="pylsqpack",
+    version="0.3.1",
+    description="Python wrapper for the ls-qpack QPACK library",
     long_description=long_description,
-    url='https://github.com/aiortc/pylsqpack',
-    author='Jeremy Lainé',
-    author_email='jeremy.laine@m4x.org',
-    license='BSD',
+    url="https://github.com/aiortc/pylsqpack",
+    author="Jeremy Lainé",
+    author_email="jeremy.laine@m4x.org",
+    license="BSD",
     classifiers=[
         "Development Status :: 4 - Beta",
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
     ],
     ext_modules=[
         setuptools.Extension(
-            'pylsqpack',
-            include_dirs=[
-                'vendor/ls-qpack',
-                'vendor/ls-qpack/deps/xxhash/',
-            ],
+            "pylsqpack",
+            include_dirs=["vendor/ls-qpack", "vendor/ls-qpack/deps/xxhash/",],
             sources=[
-                'src/module.c',
-                'vendor/ls-qpack/lsqpack.c',
-                'vendor/ls-qpack/deps/xxhash/xxhash.c',
+                "src/module.c",
+                "vendor/ls-qpack/lsqpack.c",
+                "vendor/ls-qpack/deps/xxhash/xxhash.c",
             ],
         ),
     ],
