b695f2f0898f3e31db251141e67f1a3c6070abf3
Jeremy Lainé <jeremy.laine@m4x.org>
2023-04-02 17:24:35 +0200

    Move package metadata to pyproject.toml

--- src/pylsqpack/__init__.py
+++ src/pylsqpack/__init__.py
@@ -8,4 +8,5 @@ from ._binding import (
     EncoderStreamError,
     StreamBlocked,
 )
-from .about import __version__
+
+__version__ = "0.3.16"
--- src/pylsqpack/about.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Jeremy Lainé"
-__email__ = "jeremy.laine@m4x.org"
-__license__ = "BSD"
-__summary__ = "Python wrapper for the ls-qpack QPACK library"
-__title__ = "pylsqpack"
-__uri__ = "https://github.com/aiortc/pylsqpack"
-__version__ = "0.3.16"
