3e2367210b6c24dc7b0b1ab3d780185bcafb8084
Jeremy Lainé <jeremy.laine@m4x.org>
2023-07-04 12:15:06 +0200

    Build ABI3 wheels using the Python limited API

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os.path
 import sys
 
 import setuptools
+from wheel.bdist_wheel import bdist_wheel
 
 extra_compile_args = []
 include_dirs = [
@@ -13,12 +14,25 @@ if sys.platform == "win32":
 else:
     extra_compile_args = ["-std=c99"]
 
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp38", "abi3", plat
+
+        return python, abi, plat
+
+
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             "pylsqpack._binding",
+            define_macros=[("Py_LIMITED_API", "0x03080000")],
             extra_compile_args=extra_compile_args,
             include_dirs=include_dirs,
+            py_limited_api=True,
             sources=[
                 "src/pylsqpack/binding.c",
                 "vendor/ls-qpack/lsqpack.c",
@@ -26,4 +40,5 @@ setuptools.setup(
             ],
         ),
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
